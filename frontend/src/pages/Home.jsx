2dd53f27e8cb7fcee1d71edcbfe35687e2124eeb
Mohammad Shadan <shashadan@student.iul.ac.in>
2026-09-25 03:08:03 +0530

    Remove top banner ad

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import AdBanner from "../components/AdBanner";
-import TopBanner from "../components/TopBanner";
+
 
 export default function Home() {
   const [selectedAmount, setSelectedAmount] = useState(500);
@@ -389,7 +389,7 @@ export default function Home() {
         </div>
       )}
 <AdBanner />
-<TopBanner />
+
     </div>
   );
   
